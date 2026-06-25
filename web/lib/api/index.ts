7825e53ab848860f35b2d3c9a14827d7260a0dc7
Manuel Jaime <manueljaime0020@gmail.com>
2026-06-25 19:58:55 +0100

    fix: remove old lib folder

--- web/lib/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './client';
-export * from './endpoints/auth';
-export * from './endpoints/products';
-export * from './endpoints/categories';
-export * from './endpoints/requests';
-export { API_CONFIG } from '@/shared/config/api.config';
